d543daa263b10760f37986b8e675301d845b20dc
Osama Hamad <osamahama2005@gmail.com>
2022-01-25 11:22:47 -0500

    secure cookie

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const answerRouter = require('./routes/answer')
 const userRouter = require('./routes/user')
 const issuesRouter = require('./routes/issues')
 
-// configure env and setup env variables
+// configure env file
 require('dotenv').config()
+
+// setup env variables
 const PORT = process.env.PORT
 const URI = process.env.URI
 const SESSION_SECRET = process.env.SESSION_SECRET
@@ -28,17 +30,18 @@ app.use(session(
         cookie: 
         {
             // specify cookie options
+            maxAge: 1000 * 60 * 60 * 4  // set to 4 hours
         }
     }
 ))
 
-// setup view engine
+// setup ejs view engine
 app.set('view engine', 'ejs')
 app.set('views', __dirname+'/views')
 
-// application usages
-app.use(express.static(__dirname + '/static'))      // allow static files for views
-app.use(express.json())     // middleware to parse json
+// setup static folder for css and enable parsers
+app.use(express.static(__dirname + '/static'))
+app.use(express.json())
 app.use(express.urlencoded( {extended: true} ))
 
 // use routes to make requests
@@ -48,24 +51,23 @@ app.use('/user', userRouter)
 app.use('/issues', issuesRouter)
 
 // request for homepage
-app.get('/', async (req, res)=>{
+app.get('/', async (req, res) => {
     res.redirect('/questions')
 })
 
 // request for any page that does not exist (404 error)
-app.get('*', async (req, res)=>{
+app.get('*', async (req, res) => {
     res.render('errors/404')
 })
 
-// connect to db then listen on port
-mongoose.connect(URI)
-    .then(()=>{
-        console.log('connected to db')
-        // listen to port
-        app.listen(PORT, ()=>{
-            console.log('listening on port', PORT)
-        })
+// connect to db
+mongoose.connect(URI, (e) => {
+    // return if there is error
+    if (e) return console.log(e)
+
+    // listen on port
+    console.log('connected to db')
+    app.listen(PORT, () => {
+        console.log('listening on port', PORT)
     })
-    .catch((e)=>{
-        console.log('error occured:', e)
-    })
+})
